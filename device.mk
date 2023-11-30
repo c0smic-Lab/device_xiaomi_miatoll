e35bc7e28a62ead74c7119dd988ac427da72bc0b
Giovanni Ricca <giovanniricca@proton.me>
2023-11-30 18:23:43 +0100

    miatoll: Build fpc interface from source
    Change-Id: I80dd66139d1e53873beb14f7937cbfb721e582ca

--- device.mk
+++ device.mk
@@ -157,6 +157,7 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 PRODUCT_PACKAGES += \
+    com.fingerprints.extension@1.0.vendor \
     libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # Framework detect
