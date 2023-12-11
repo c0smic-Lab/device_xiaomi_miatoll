978cdcecae8537b1e1b084b4dfa8f98848ad8554
electimon <electimon@gmail.com>
2023-12-11 12:56:46 +0100

    miatoll: Move sscrpcd to class core
    * If sscrpcd start early, the dsp it runs on
      isn't ready yet.
    
    test: Notice reduction of
          "Error 0x200: apps_dev_init failed for domain 0, errno No such device"
          in early boot logcat.
    Change-Id: Ib3ec232677e4831262de631f5ccd9dfa6008f6a0
    Signed-off-by: electimon <electimon@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,9 @@ function blob_fixup() {
         vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc)
             sed -i "s/4\.0/4\.1/g" "${2}"
             ;;
+        vendor/etc/init/vendor.sensors.sscrpcd.rc)
+            sed -i 's|class early_hal|class core|g' "${2}"
+            ;;
         vendor/lib64/camera/components/com.qti.node.watermark.so)
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
