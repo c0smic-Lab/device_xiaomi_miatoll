7c6a30dbb99bce01fb112a22facb65acb979b440
COSMIC <rayavanindra@gmail.com>
2024-02-10 20:04:56 +0530

    Revert "sm6250-common: Import powerhint.json"
    This reverts commit a1d3084060022f75ea6073b87c4e8722b1965f48.

--- device.mk
+++ device.mk
@@ -395,9 +395,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
