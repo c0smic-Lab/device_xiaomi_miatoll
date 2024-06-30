7ddd64229426c43c87ffed6971dc73b8ff400161
0xCAFEBABE <me.cafebabe@gmail.com>
2024-06-30 20:42:43 +0800

    miatoll: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- device.mk
+++ device.mk
@@ -175,6 +175,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    SM6250DeviceAsWebcam \
     SM6250Frameworks \
     SM6250Settings \
     SM6250SystemUI \
