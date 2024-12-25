ce87358dff000eaa239578587570c6aa0ed705f2
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    miatoll: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- device.mk
+++ device.mk
@@ -169,6 +169,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    NcmTetheringOverlay \
     SM6250DeviceAsWebcam \
     SM6250Frameworks \
     SM6250Settings \
