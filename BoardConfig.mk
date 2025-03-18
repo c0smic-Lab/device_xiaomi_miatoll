0e0c06b36e4d5f9ce1b2e42c1edb89db7212d7ae
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-18 18:05:43 +0800

    miatoll: Drop `TARGET_RECOVERY_DEVICE_MODULES`
    * It's deprecated on 15 QPR2
    * Building a library as module doesn't get it executed? and libinit
      still does its job in recovery mode without migrating this
    
    Change-Id: I7be000356fe7d7bd099615d334892d1686bece0e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,7 +11,6 @@ DEVICE_PATH := device/xiaomi/miatoll
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_miatoll
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_miatoll
 
 # MiuiCamera
 -include vendor/xiaomi/miuicamera/BoardConfigMiuiCamera.mk
